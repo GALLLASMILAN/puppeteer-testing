ba7a92d81f1be11d48836d7b89b981602e56b776
Zac Jones <zacjones93@gmail.com>
2018-03-30 13:41:25 -0700

    modified for lesson-1

--- src/App.test.js
+++ src/App.test.js
@@ -3,40 +3,24 @@ const puppeteer = require('puppeteer')
 const isDebugging = () => {
   let debugging_mode = {
     headless: false,
-    slowMo: 50,
+    slowMo: 250,
     devtools: true  
   }
   return process.env.NODE_ENV === 'debug' ? debugging_mode : {};
 }
 
-let browser
-let page
-beforeAll(async () => {
-  browser = await puppeteer.launch(isDebugging())
-  page = await browser.newPage()
-  await page.goto('http://localhost:3000/')
-  page.setViewport({ width: 500, height: 2400 })
-})
-
 describe('on page load ', () => {
   test('h1 loads correctly', async () => {
-    const html = await page.$eval('[data-testid="h1"]', e => e.innerHTML)
+    let browser = await puppeteer.launch(isDebugging())
+    let page = await browser.newPage()
+  
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400
+      },
+      userAgent: ''
+    })
 
-    expect(html).toBe('Welcome to React')
-
-  }, 16000)
-
-  test('nav loads correctly', async () => {
-    const navbar = await page.$eval('[data-testid="navbar"]', el => el ? true : false)
-    const listItems = await page.$$('[data-testid="navBarLi"]')
-
-    expect(navbar).toBe(true)
-    expect(listItems.length).toBe(4)
   })
-})
-
-afterAll(() => {
-  if (isDebugging()) {
-    browser.close()
-  }
 })
